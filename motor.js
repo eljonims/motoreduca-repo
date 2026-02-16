72319105b6ead0d32489abab0ae4f56f1fe9e417
Jonathan Marin <schanejin@gmail.com>
2026-02-16 21:48:57 +0100

    Añadida conexión inicial a indexedDB

--- motor.js
+++ motor.js
@@ -21,22 +21,27 @@ class MotorEduca {
         this.bitacora("Despertando MotorEduca...", 10);
 
         try {
+            this.bitacora("Conectando al almacén local...", 20);
+            this.db = await this.conectarAlmacen();
+            await this.esperar(500);
+            this.bitacora("[OK] Almacén IndexedDB listo.", 25);
+            // ---------------------------------------
             this.bitacora(`Conectando con fuente: ${url}`, 30);
             const respuesta = await fetch(url);
-            await this.esperar(600); 
+            await this.esperar(600);
 
             if (!respuesta.ok) throw new Error("Fitxategia ez da aurkitu");
 
             this.bitacora("Descargando temario...", 60);
             this.data = await respuesta.json();
-            await this.esperar(1000); 
+            await this.esperar(1000);
 
             this.bitacora("Validando integridad...", 80);
-            await this.esperar(500); 
+            await this.esperar(500);
             // Aquí iría tu validarEsquema() en el futuro
 
             this.bitacora("¡Todo listo! Arrancando...", 100);
-            await this.esperar(800); 
+            await this.esperar(800);
 
             setTimeout(() => {
                 document.getElementById('pantalla-lanzamiento').style.opacity = "0";
@@ -50,6 +55,28 @@ class MotorEduca {
         } catch (error) {
             this.bitacora(`ERROR CRÍTICO: ${error.message}`, 100);
         }
+    }
+    conectarAlmacen() {
+        return new Promise((resolver, rechazar) => {
+            // Abrimos la BD "MotorEduca_DB" versión 1
+            const peticion = indexedDB.open("MotorEduca_DB", 1);
+
+            // Si es la primera vez (o subimos versión), creamos las "estanterías"
+            peticion.onupgradeneeded = (e) => {
+                const db = e.target.result;
+                // Estantería 1: Los libros (el JSON completo)
+                if (!db.objectStoreNames.contains("libros")) {
+                    db.createObjectStore("libros", { keyPath: "id" });
+                }
+                // Estantería 2: El progreso (vistos, completados)
+                if (!db.objectStoreNames.contains("progreso")) {
+                    db.createObjectStore("progreso", { keyPath: "id_libro" });
+                }
+            };
+
+            peticion.onsuccess = (e) => resolver(e.target.result);
+            peticion.onerror = (e) => rechazar("Error abriendo Almacén");
+        });
     }
     esperar(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
